5523d4646415425de68a0cf37dcba96fcefa519d
xzh <anenan@qq.com>
2021-04-13 10:16:42 +0800

    无重复字符的最长子串

--- src/main/java/com/xzh/solution/无重复字符的最长子串.java
+++ src/main/java/com/xzh/solution/无重复字符的最长子串.java
@@ -1,5 +1,7 @@
 package com.xzh.solution;
 
+import java.util.HashMap;
+
 /**
  * @author 向振华
  * @date 2021/04/12 16:34
@@ -10,19 +12,16 @@ public class 无重复字符的最长子串 {
         if (s.length() == 0) {
             return 0;
         }
-        int maxLen = 0;
-        String max = "";
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0, left = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (!max.contains(String.valueOf(s.charAt(i)))) {
-                max += s.charAt(i);
-            } else {
-                max = max.substring(max.indexOf(s.charAt(i)) + 1) + s.charAt(i);
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
             }
-            if (max.length() > maxLen) {
-                maxLen = max.length();
-            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
         }
-        return maxLen;
+        return max;
     }
 
     public static void main(String[] args) {

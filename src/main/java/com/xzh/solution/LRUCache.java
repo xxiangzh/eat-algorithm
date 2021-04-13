8664d2fc1c5d0cf52dea42a67a409c1ee7c18ff9
xzh <anenan@qq.com>
2021-04-13 10:28:03 +0800

    LRU缓存机制

---
+++ src/main/java/com/xzh/solution/LRUCache.java
@@ -0,0 +1,31 @@
+package com.xzh.solution;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author 向振华
+ * @date 2021/04/13 10:17
+ */
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
